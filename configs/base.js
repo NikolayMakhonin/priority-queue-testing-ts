c0dc0683fb2e42584481c96a44570db193057a70
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2020-03-06 16:21:49 +0200

    Node.js module completed

--- configs/base.js
+++ configs/base.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-process-env */
 module.exports = {
-	appId      : 'com.app-template',
-	packageName: 'app-template',
+	appId      : 'com.priority-queues',
+	packageName: 'priority-queues',
 	appName    : 'App Template',
 	appVersion : '0.0.1',
 	description: 'App Template',
 	logUrls    : [
-		// 'http://app-template.logger.com/log.php', // TODO
+		// 'http://priority-queues.logger.com/log.php', // TODO
 	],
 	installer  : {
 		electronVersion: '6.0.11',
